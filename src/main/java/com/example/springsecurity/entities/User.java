d3c5df6891262b9bfe89fa332a8a1daf910cdae5
Traums <dmitriy-martyushev@mail.ru>
2024-02-12 18:10:40 +0500

    Initial

---
+++ src/main/java/com/example/springsecurity/entities/User.java
@@ -0,0 +1,32 @@
+package com.example.springsecurity.entities;
+
+import jakarta.persistence.*;
+import lombok.Data;
+
+import java.util.Collection;
+import java.util.Collections;
+
+@Entity
+@Data
+@Table(name = "users")
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private Integer id;
+
+    @Column(name = "username")
+    private String username;
+
+    @Column(name = "password")
+    private String password;
+
+    @Column(name = "email")
+    private String email;
+
+    @ManyToMany
+    @JoinTable(name = "users_roles",
+    joinColumns = @JoinColumn(name = "user_id"),
+    inverseJoinColumns = @JoinColumn(name = "role_id"))
+    private Collection<Role> roles;
+}
